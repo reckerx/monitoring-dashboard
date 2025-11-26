b13b6369a8ce8bc59d4332aafe916ffbeabffdae
reckerx <40579178+reckerx@users.noreply.github.com>
2025-11-26 12:46:02 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 // src/api.js
 import axios from "axios";
-
-const API_BASE_URL = "http://localhost:30080";
+const API_BASE_URL =
+  (window.RUNTIME_CONFIG && window.RUNTIME_CONFIG.REACT_APP_API_BASE_URL) ||
+  "http://localhost:30080";
+// const API_BASE_URL = "http://localhost:30080";
 // const API_BASE_URL = window.RUNTIME_CONFIG.REACT_APP_API_BASE_URL
 export const fetchMetrics = async () => {
   const response = await axios.get(`${API_BASE_URL}/metrics`);
